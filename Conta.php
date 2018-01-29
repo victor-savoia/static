2d8f980d58bd6f35100ee0e230ccf191e59e7f57
php 19 <php.19@catatau.cec.ime.usp.br>
2018-01-29 20:19:06 -0200

    Metodo de classe

--- Conta.php
+++ Conta.php
@@ -3,9 +3,16 @@
 class Conta {
 
   public static $contador;
+  public $numeroConta;
 
   function __construct(){
       self::$contador++;
+      $this->numeroConta = self::$contador;
+  }
+
+  static function  zeraContador(){
+    echo 'Total de contas anteriores: ' . self::$contador.PHP_EOL;
+    self::$contador = 0;
   }
 
 }
--- testaConta.php
+++ testaConta.php
@@ -2,12 +2,16 @@
 
   require ('Conta.php');
 
-  echo Conta::$contador . PHP_EOL;
-  $c = new Conta();
-  echo Conta::$contador. PHP_EOL;
 
-  $c = new Conta();
-  echo Conta::$contador. PHP_EOL;
+  $c1 = new Conta();
+  echo $c1->numeroConta . PHP_EOL;
+  $c2 = new Conta();
+  echo $c2->numeroConta . PHP_EOL;
+  $c3 = new Conta();
+  echo $c3->numeroConta . PHP_EOL;
 
+  Conta::zeraContador();
+
+  echo 'Contador zerado: ' . Conta::$contador . PHP_EOL;
 
 ?>
